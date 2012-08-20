ad4a1304e7977b8ed1b8be1ead4f512928715b65
Chris Oates <co@digitaleskimo.net>
2012-08-20 15:57:07 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, find_packages
+
+setup(name='facebook-python-sdk',
+      version='1.0',
+      description='Backup of defunct version of Facebook Python SDK',
+      author='',
+      author_email='',
+      url='http://github.com/digital-eskimo/fb-python-sdk',
+      packages=find_packages()
+     )
